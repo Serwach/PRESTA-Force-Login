cfbac0800c8e63708937122335504d9fecb83706
Mateusz Serwach <mateusz.serwach@supremis.pl>
2025-03-03 16:12:08 +0100

    forcing login - backend work done

---
+++ forcelogin/forcelogin.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class ForceLogin extends Module
+{
+    /**
+     * @var array
+     */
+    private $allowedControllers = ['authentication', 'password'];
+
+    public function __construct()
+    {
+        $this->name = 'forcelogin';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Mateusz Serwach';
+        $this->need_instance = 0;
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Force Login');
+        $this->description = $this->l('B2B module for forcing login (hiding the content for B2C)');
+    }
+
+    public function install(): bool
+    {
+        return parent::install() && $this->registerHook('actionDispatcher');
+    }
+
+    /**
+     * @param array $params
+     */
+    public function hookActionDispatcher(array $params): void
+    {
+        if ($this->context->customer->isLogged()) {
+            return;
+        }
+
+        $currentController = Dispatcher::getInstance()->getController();
+
+        if (!$this->isAllowedController($currentController)) {
+            Tools::redirect($this->context->link->getPageLink('authentication', true));
+        }
+    }
+
+    /**
+     * @param string $controller
+     * @return bool
+     */
+    private function isAllowedController(string $controller): bool
+    {
+        return in_array(strtolower($controller), $this->allowedControllers, true);
+    }
+}
